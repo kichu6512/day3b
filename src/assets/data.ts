ed1488479bc99e9ad1af7944325ea701982c6063
kichu6512 <vishnu.prc20cs049@providence.edu.in>
2023-03-03 09:35:23 +0530

    first commit

---
+++ src/assets/data.ts
@@ -0,0 +1,70 @@
+export const data = [
+    {
+        title:"TVM",
+        image:"w1.jpg"
+        
+    },
+    {
+        title:"Kollam",
+        image:"ca3.jpg"
+    },{
+        title:"PTA",
+        image:"KANNUR.jpg"
+    },
+    {
+        title:"Kasargod",
+        image:"d1.jpg"
+        
+    },
+    {
+        title:"Kannur",
+        image:"m1.jpeg"
+        
+    },
+    {
+        title:"Kozhikode",
+        image:"l1.jpeg"
+        
+    },
+    {
+        title:"Wayanad",
+        image:"ca3.jpg"
+        
+    },
+    {
+        title:"Malappuram",
+        image:"m1.jpeg"
+        
+    },
+    {
+        title:"Palakkad",
+        image:"w1.jpg"
+        
+    },
+    {
+        title:"Thrissur",
+        image:"m1.jpeg"
+        
+    },
+    {
+        title:"Ernakulam",
+        image:"l1.jpeg"
+        
+    },
+    {
+        title:"Idukki",
+        image:"d1.jpg"
+        
+    },
+    {
+        title:"Kottayam",
+        image:"l1.jpeg"
+        
+    },
+    {
+        title:"Alappuzha",
+        image:"d1.jpg"
+        
+    },
+    
+]
